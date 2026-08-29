b90ea43d0b479fd9e96bde220a9b9559b2b565e6
rumahsoftware551 <rumahsoftware551@gmail.com>
2026-08-29 10:18:29 +0700

    Add ISPfinance V1 navigation structure

---
+++ admin/ispfinance_menu.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * ISPfinance V1.0 Main Navigation
+ */
+?>
+<ul class="sidebar-menu">
+    <li><a href="index.php">Dashboard</a></li>
+
+    <li class="treeview">
+        <a href="#">Keuangan</a>
+        <ul>
+            <li><a href="#">Kas & Bank</a></li>
+            <li><a href="#">Transaksi</a></li>
+            <li><a href="#">Utang & Piutang</a></li>
+        </ul>
+    </li>
+
+    <li class="treeview">
+        <a href="#">Tagihan</a>
+        <ul>
+            <li><a href="#">Pelanggan</a></li>
+            <li><a href="#">Invoice</a></li>
+            <li><a href="#">Pembayaran</a></li>
+        </ul>
+    </li>
+
+    <li class="treeview">
+        <a href="#">Inventory</a>
+        <ul>
+            <li><a href="#">Database Barang</a></li>
+            <li><a href="#">Stock Gudang</a></li>
+            <li><a href="#">Movement</a></li>
+            <li><a href="#">Outstanding</a></li>
+        </ul>
+    </li>
+
+    <li><a href="#">Laporan</a></li>
+    <li><a href="#">Settings</a></li>
+</ul>
